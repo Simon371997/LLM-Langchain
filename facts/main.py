5daa283140e74ad85aae9b662a383c9f916ef8ab
Simon Henes <simon.henes@live.de>
2024-07-08 18:17:33 +0200

    facts project

---
+++ facts/main.py
@@ -0,0 +1,27 @@
+"""Facts"""
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+
+emb = embeddings.embed_query("")
+
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=200,
+    chunk_overlap=0
+)
+
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(
+    text_splitter=text_splitter
+)
+
+for doc in docs:
+    print(doc.page_content)
+    print("\n")
+
