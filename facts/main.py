e34f242ab2c294ab0936f73446dff1512fb162c2
Simon Henes <simon.henes@live.de>
2024-07-09 14:45:08 +0200

    facts project (issue)

--- facts/main.py
+++ facts/main.py
@@ -1,27 +1,26 @@
-"""Facts"""
-from langchain.document_loaders import TextLoader
+""" main file """
+
+from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
 
 embeddings = OpenAIEmbeddings()
 
-emb = embeddings.embed_query("")
-
-text_splitter = CharacterTextSplitter(
-    separator="\n",
-    chunk_size=200,
-    chunk_overlap=0
-)
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=200, chunk_overlap=0)
 
 loader = TextLoader("facts.txt")
-docs = loader.load_and_split(
-    text_splitter=text_splitter
+docs = loader.load_and_split(text_splitter=text_splitter)
+
+db = Chroma.from_documents(docs, embedding=embeddings, persist_directory="emb")
+
+results = db.similarity_search(
+    "What is an interesting fact about the English language?"
 )
 
-for doc in docs:
-    print(doc.page_content)
+for result in results:
     print("\n")
-
+    print(result.page_content)
