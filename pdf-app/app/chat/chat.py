e5cb42fcaa900815cb0659914c6400de425791ed
Simon Henes <simon.henes@live.de>
2024-07-16 12:51:01 +0200

    implement StreamingChain

--- pdf-app/app/chat/chat.py
+++ pdf-app/app/chat/chat.py
@@ -1,9 +1,10 @@
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pineconce import build_retriever
 from app.chat.llms.chatopenai import build_llm
 from app.chat.memories.sql_memory import build_memory
+from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
 
 
 def build_chat(chat_args: ChatArgs):
@@ -20,8 +21,12 @@ def build_chat(chat_args: ChatArgs):
 
     retriever = build_retriever(chat_args=chat_args)
     llm = build_llm(chat_args=chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args=chat_args)
 
-    return ConversationalRetrievalChain.from_llm(
-        llm=llm, memory=memory, retriever=retriever
+    return StreamingConversationalRetrievalChain.from_llm(
+        llm=llm,
+        condense_question_llm=condense_question_llm,
+        memory=memory,
+        retriever=retriever,
     )
